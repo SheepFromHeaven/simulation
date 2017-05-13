04e63a873ab448fc1046d5fd759418c91165a8ac
Marc Wißler <marc.wissler@virtual-identity.com>
2017-05-13 16:32:28 +0200

    Add Phaser

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-console.log('Hello World');
+import 'pixi';
+import 'p2';
+import * as Phaser from 'phaser';
+
+class App {
+
+    constructor() {
+        this.game = new Phaser.Game(window.outerWidth, window.outerHeight, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+
+    game: Phaser.Game;
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+}
+
+window.onload = () => {
+
+    let game = new App();
+
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const path = require('path');
 const webpack = require('webpack');
+
+const phaserModule = path.join(__dirname, '/node_modules/phaser/');
+const phaser = path.join(phaserModule, 'build/custom/phaser-split.js');
+const pixi = path.join(phaserModule, 'build/custom/pixi.js');
+const p2 = path.join(phaserModule, 'build/custom/p2.js');
 
 module.exports = {
     entry: './src/app.ts',
@@ -13,17 +19,28 @@ module.exports = {
             'node_modules',
             path.resolve(__dirname, 'src')
         ],
-        extensions: ['.ts', '.js']
+        extensions: ['.ts', '.js'],
+        alias: {
+            'phaser': phaser,
+            'pixi': pixi,
+            'p2': p2
+        }
     },
     module: {
         rules: [
             {
                 test: /\.ts?$/,
                 include: [path.resolve(__dirname, 'src')],
-                exclude: [],
                 loader: 'ts-loader'
-            }
+            },
+            { test: /pixi\.js/, loader: 'expose-loader?PIXI' },
+            { test: /phaser-split\.js$/, loader: 'expose-loader?Phaser' },
+            { test: /p2\.js/, loader: 'expose-loader?p2' }
         ]
+    },
+
+    node: {
+        fs: 'empty'
     },
 
     devServer: {
@@ -34,6 +51,17 @@ module.exports = {
     },
 
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'node-static',
+            filename: 'node-static.js',
+            minChunks: function(module, count) {
+                var context = module.context;
+                return context && context.indexOf('node_modules') >= 0;
+            }
+        })
     ]
 };
