e8d9969011a5e3e6d02e3b27c83cf8b656df1e85
SheepFromHeaven <marcwissler@gmail.com>
2017-06-29 17:09:31 +0200

    Changed everything to FP and redux

---
+++ src/interfaces/Building.ts
@@ -0,0 +1,6 @@
+import {BUILDING_TYPE} from '../types/BUILDING_TYPES';
+
+export interface Building {
+    id: number;
+    type: BUILDING_TYPE;
+}
--- src/interfaces/EntityManager.ts
+++
@@ -1,7 +0,0 @@
-import {Entity} from '../models/Entity';
-
-export interface EntityManager {
-    registerEntity: (Entity) => void,
-    getAllEntities: () => Entity[],
-    getEntityById: (number) => Entity
-}
