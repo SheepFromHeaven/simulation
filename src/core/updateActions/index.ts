83b5f787382ca9fe0181f370f6c27eba01fcbeed
SheepFromHeaven <marcwissler@gmail.com>
2017-09-24 10:32:57 +0200

    Fix nodemon config

--- src/core/updateActions/index.ts
+++ src/core/updateActions/index.ts
@@ -2,7 +2,6 @@ import {increaseTick} from '../game/game.actions';
 import {updateProduction} from './production';
 import {ThunkAction} from 'redux-thunk';
 import {ApplicationState} from '../AppState';
-import {RESOURCE_TYPE} from '../types/RESOURCE_TYPES';
 
 export const updateAction = (): ThunkAction<void, ApplicationState, {}> =>
   (dispatch, getState) => {
@@ -10,7 +9,6 @@ export const updateAction = (): ThunkAction<void, ApplicationState, {}> =>
     dispatch(increaseTick());
     const currentState = getState();
     if(currentState.game.ticksPassed !== originalState.game.ticksPassed) {
-      console.log(currentState.resources.byId[RESOURCE_TYPE.WOOD]);
       updateProduction(dispatch, currentState);
     }
   };
